15f28c8e7e2f32b66f1ad5234372e5b487a909d2
Demchyshyn <demchyshyn.v87@gmail.com>
2021-01-23 03:18:47 +0200

    add comments to interceptor

--- frontend/src/app/main.interceptor.ts
+++ frontend/src/app/main.interceptor.ts
@@ -17,41 +17,37 @@ export class MainInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService, private router: Router) {
   }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const isAuthenticated = this.authService.isAuthenticated();
-    if (isAuthenticated) {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const isAuthenticated = this.authService.isAuthenticated(); // определяем есть ли у нас access токен в localstorage
+    if (isAuthenticated) { // если есть то сетаем его в header
       request = this.addToken(request, this.authService.getAccessToken());
     }
-    return next.handle(request).pipe(
-      catchError((res: HttpErrorResponse) => {
-        if (res && res.error) {
-          if (res instanceof HttpErrorResponse && res.status === 401) {
-            return this.handle401Error(request, next);
-          }
-          if (res.status === 403) {
-            this.router.navigate(['login'], {
-              queryParams: {
-                sessionFiled: true
-              }
-            });
-          }
+    return next.handle(request).pipe(catchError((res: HttpErrorResponse) => { // отсылаем реквест на сервер попутно отслеживаем ошибки
+      if (res && res.error) {
+        if (res instanceof HttpErrorResponse && res.status === 401) {
+          return this.handle401Error(request, next); // если 401 ошибка переходим в метод обработки ошибки
         }
-      })
-    ) as any;
+        console.log(res.error.detail);
+      }
+      if (res.status === 403) { // если 403 ошибка переходим на страницу логинации
+        this.router.navigate(['login'], {
+          queryParams: {
+            sessionFiled: true
+          }
+        });
+      }
+    })) as any;
   }
 
-  addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
-    return request.clone({
-      setHeaders: {Authorization: `Bearer ${token}`}
-    });
+  addToken(request: HttpRequest<any>, token: string): HttpRequest<any> { // метод для добавления в request header с токеном
+    return request.clone({setHeaders: {Authorization: `Bearer ${token}`}});
   }
 
-  private handle401Error(request: HttpRequest<any>, next: HttpHandler): any {
-    return this.authService.refreshToken().pipe(
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler): any { // метод для обработки 401 ошибки
+    return this.authService.refreshToken().pipe( // отсылаем запрос на бек на рефреш
       switchMap((token: IToken) => {
-        return next.handle(this.addToken(request, token.access));
+        return next.handle(this.addToken(request, token.access)); // передаем дальше реквест с новым accessToken
       })
     );
   }
-
 }
